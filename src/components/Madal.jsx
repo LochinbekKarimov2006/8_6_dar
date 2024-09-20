a9c81385b9e196cc0726461b2ff7ff3dbaf0a439
LochinbekKarimov2006 <161306293+LochinbekKarimov2006@users.noreply.github.com>
2024-09-20 16:36:31 +0500

    first commit

---
+++ src/components/Madal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Madal() {
+  return (
+    <div>Madal</div>
+  )
+}
+
+export default Madal
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Madal from '../components/Madal'
+
+function Home() {
+  return (
+    <div><button>
+        </button>
+        </div>
+  )
+}
+
+export default Home
