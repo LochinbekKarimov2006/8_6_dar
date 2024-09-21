e12d67f15c2a30b8d6220a9a2b2949784856d36c
LochinbekKarimov2006 <161306293+LochinbekKarimov2006@users.noreply.github.com>
2024-09-21 17:06:30 +0500

    navbar tugadi

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Menyu from '../components/Menyu'
 import { Outlet } from 'react-router-dom'
+import Menyu from '../components/Menyu'
 
 function MainLayout() {
   return (
-    <div className='h-[100vh] bg-slate-500'>
-        <Navbar/>
-        <div>
-            <Menyu/>
-            <Outlet/>
-        </div>
+    <div className='flex h-[100vh] '>
+        <Menyu/>
+         <div className='w-[100%]'>
+           <Navbar/>
+           <div className='overflow-y-auto h-[80vh]'>
+           <Outlet/>
+           </div>
+         </div>
     </div>
+   
   )
 }
 
