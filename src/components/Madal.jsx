bd2af40a0b0db6b8c836c40e2ab92108b3e3141c
LochinbekKarimov2006 <161306293+LochinbekKarimov2006@users.noreply.github.com>
2024-09-23 04:19:51 +0500

    .

--- src/components/Madal.jsx
+++ src/components/Madal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Madal() {
   return (
-    <div>salom</div>
+    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae iste quibusdam earum illum tempore consectetur qui cum in, neque libero!</div>
   )
 }
 
