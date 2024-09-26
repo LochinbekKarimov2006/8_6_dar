74dd87517da3a8ae7b770f2630355140f06494f9
splinter wow <splinterwow01@gmail.com>
2024-09-26 12:16:43 +0500

    register and login done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import React from 'react'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import MainLayout from './layout/MainLayout'
-import Home from './pages/Home'
-import AboutUser from './pages/User'
+import React from "react";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import AboutUser from "./pages/User";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ErrorPage from "./pages/ErrorPage";
+
+const isAuthenticated = () => {
+  return localStorage.getItem("token") !== null;
+};
+
+const PrivateRoute = ({ element }) => {
+  return isAuthenticated() ? element : <Navigate to="/login" />;
+};
+
 function App() {
-  const router=createBrowserRouter([
+  const router = createBrowserRouter([
     {
-      path:"/",
-      element:<MainLayout/>,
-      children:[
+      path: "/",
+      element: <MainLayout />,
+      errorElement: <ErrorPage />,
+      children: [
         {
-          path:"/",
-          element:<Home/>
+          path: "/",
+          element: <PrivateRoute element={<Home />} />,
         },
         {
-          path:"/user",
-          element:<AboutUser/>
-        }
-      ]
-    }
-  ])
-  return (
-    <RouterProvider router={router}/>
-  )
+          path: "/user",
+          element: <AboutUser />,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "*",
+      element: <ErrorPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
